69550dfa5db53e8167c5f3c0ffd9ecc3fdc6e06e
Claudio Miranda <claudio@febos.cl>
2018-08-29 23:26:11 -0300

    Primeras paginas

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { AuthServiceProvider } from './../providers/auth-service/auth-service';
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
+
+@NgModule({
+  declarations: [
+    MyApp
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthServiceProvider
+  ]
+})
+export class AppModule {}
+
